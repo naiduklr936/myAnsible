74e98b37e96690bffe66ed85dc8c7bab1a639185
naiduklr936 <naiduklr936@gmail.com>
2017-04-28 13:58:13 -0500

    Add files via upload

---
+++ roles/jenkins/templates/jdk-install.groovy
@@ -0,0 +1,16 @@
+import hudson.model.JDK
+import hudson.tools.JDKInstaller
+import hudson.tools.InstallSourceProperty
+import jenkins.model.Jenkins
+
+def descriptor = new JDK.DescriptorImpl();
+
+if (descriptor.getInstallations()) {
+    println 'skip jdk installations'
+} else {
+    println 'add jdk8'
+    Jenkins.instance.updateCenter.getById('default').updateDirectlyNow(true)
+    def jdkInstaller = new JDKInstaller('jdk-8u131-oth-JPR', true)
+    def jdk = new JDK("jdk8", null, [new InstallSourceProperty([jdkInstaller])])
+    descriptor.setInstallations(jdk)
+}
